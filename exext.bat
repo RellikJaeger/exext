3bbc35af4308b5403490b25158c2ed0a281f2657
Rellik Jaeger <rellikjaeger@gmail.com>
2022-08-28 02:35:14 +0630

    Add files via upload

---
+++ exext.bat
@@ -0,0 +1,20 @@
+@echo off
+if [%1]==[] echo 1st argument (original extension) is null && goto :eg
+if [%2]==[] echo 2nd argument (target extension) is null && goto :eg
+setlocal enabledelayedexpansion
+for %%i in (*.%1) do (
+	set test=%%i
+	echo.
+	echo ffmpeg -i "!test!" "!test:~0,-4!.%2"
+	echo.
+	ffmpeg -i "!test!" "!test:~0,-4!.%2"
+)
+endlocal
+goto :eof
+:eg
+echo.
+echo Example: exext jpg png
+echo.
+echo ffmpeg -i *.jpg *.png
+echo.
+:eof
