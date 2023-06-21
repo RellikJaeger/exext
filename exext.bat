5c3cc6759ce339b9743192fa14ec61efc1b9d1d9
Rellik Jaeger <rellikjaeger@gmail.com>
2023-06-22 01:19:49 +0630

    Update exext.bat

--- exext.bat
+++ exext.bat
@@ -1,20 +1,33 @@
 @echo off
+setlocal enabledelayedexpansion
+
 if [%1]==[] echo 1st argument (original extension) is null && goto :eg
 if [%2]==[] echo 2nd argument (target extension) is null && goto :eg
-setlocal enabledelayedexpansion
-for %%i in (*.%1) do (
-	set test=%%i
-	echo.
-	echo ffmpeg -hide_banner -y -threads 4 -hwaccel auto -i "!test!" "!test:~0,-4!.%2"
-	echo.
-	ffmpeg -hide_banner -y -threads 4 -hwaccel auto -i "!test!" "!test:~0,-4!.%2"
+
+if [%2]==[avif] (
+	for %%f in (*.%1) do (
+		echo.
+		echo ffmpeg -hide_banner -y -threads 4 -hwaccel auto -i "%%f" -c:v librav1e -qp 100 "%%~nf.%2"
+		echo.
+		ffmpeg -hide_banner -y -threads 4 -hwaccel auto -i "%%f" -c:v librav1e -qp 100 "%%~nf.%2"
+	)
+	goto :eof
 )
-endlocal
+
+for %%f in (*.%1) do (
+	echo.
+	echo ffmpeg -hide_banner -y -threads 4 -hwaccel auto -i "%%f" "%%~nf.%2"
+	echo.
+	ffmpeg -hide_banner -y -threads 4 -hwaccel auto -i "%%f" "%%~nf.%2"
+)
+
 goto :eof
+
 :eg
 echo.
 echo Example: exext jpg png
 echo.
 echo ffmpeg -i *.jpg *.png
 echo.
+
 :eof
